c26b53a58166ea833ed7c0a6f424372a43540eb4
mangojellysolutions <49659915+mangojellysolutions@users.noreply.github.com>
2020-08-07 20:24:30 +0100

    Added two macros from video series.

---
+++ AnimationVideoOutput/aviout.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+cd c:\temp
+set /p framerate=Enter framerate:
+for /r %%a in (*.jpg) do (
+    echo file '%%a' >> images.txt
+)
+c:\temp\ffmpeg.exe -r %framerate% -f concat -safe 0 -i images.txt -c:v libx264 -pix_fmt yuv420p video.mp4
+:PROMPT
+SET /P AREYOUSURE=Are you sure (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END 
+  del /q *.jpg
+:END
+del /q images.txt
+endlocal
