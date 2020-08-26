ce250bf58628584b7a70c9334dcc44ab95093b9e
mangojellysolutions <49659915+mangojellysolutions@users.noreply.github.com>
2020-08-26 17:33:38 +0100

    Forgot to push changes.

--- AnimationVideoOutput/aviout.bat
+++ AnimationVideoOutput/aviout.bat
@@ -7,7 +7,7 @@ for /r %%a in (*.jpg) do (
 )
 c:\temp\ffmpeg.exe -r %framerate% -f concat -safe 0 -i images.txt -c:v libx264 -pix_fmt yuv420p video.mp4
 :PROMPT
-SET /P AREYOUSURE=Are you sure (Y/[N])?
+SET /P AREYOUSURE=Are you sure you want to delete the source jpegs (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END 
   del /q *.jpg
 :END
